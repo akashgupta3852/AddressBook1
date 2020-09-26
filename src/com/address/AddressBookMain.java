01d15f2427f3c69b2ad873cff35e77bdca607d1c
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-26 15:40:10 +0530

    [Akash]Added: contact person class

--- src/com/address/AddressBookMain.java
+++ src/com/address/AddressBookMain.java
@@ -1,57 +1,31 @@
 package com.address;
+import java.util.*;
 
 public class AddressBookMain {
-
-	private String firstName, lastName, address, city, state;
-	private int zip, phoneNo;
-	private String emailId;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public int getZip() {
-		return zip;
-	}
-
-	public int getPhoneNo() {
-		return phoneNo;
-	}
-
-	public String getEmailId() {
-		return emailId;
-	}
-
-	public AddressBookMain(String firstName, String lastName, String address, String city, String state, int zip,
-			int phoneNo, String emailId) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phoneNo = phoneNo;
-		this.emailId = emailId;
-	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first name:");
+		String firstName=sc.next();
+		System.out.println("Enter the last name:");
+		String lastName=sc.next();
+		System.out.println("Enter the address:");
+		sc.nextLine();
+		String address=sc.nextLine();
+		System.out.println("Enter the city:");
+		String city=sc.nextLine();
+		System.out.println("Enter the state:");
+		String state=sc.nextLine();
+		System.out.println("Enter the zip:");
+		int zip=sc.nextInt();
+		System.out.println("Enter the phoneNo:");
+		long phoneNo=sc.nextLong();
+		System.out.println("Enter the email:");
+		String emailId=sc.next();
+		
+		ContactPerson personDetails=new ContactPerson(firstName,lastName,address,city,state,zip,phoneNo,emailId);
+		System.out.println(personDetails);
 	}
 
 }
